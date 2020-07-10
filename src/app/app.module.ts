775e274f7d008b5bd379dd1d60a716b31b4aea19
Sambhaji Mane (CC313640) <sambhajim@nedbank.co.za>
2020-07-10 18:54:26 +0200

    Removed the angular material library, as grid layout is not available

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,6 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatSliderModule } from '@angular/material/slider';
 
 @NgModule({
   declarations: [
@@ -12,9 +10,7 @@ import { MatSliderModule } from '@angular/material/slider';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule,
-    MatSliderModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
